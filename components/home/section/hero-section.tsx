7ee9b6f1747c88448538d2c60c762164a127b74a
SeedONova <putubaguswidia@gmail.com>
2024-10-30 23:31:52 +0700

    readjust image height

--- components/home/section/hero-section.tsx
+++ components/home/section/hero-section.tsx
@@ -4,7 +4,7 @@ import ExperienceSection from "./experience-section";
 
 export default function HeroSection() {
   return (
-<section className="relative z-0 w-full h-screen max-h-[800px] px-20 pb-16 text-white">
+<section className="relative z-0 w-full h-screen max-h-[850px] px-20 pb-16 text-white">
       <Image
         src={"/assets/img/home-page-hero-banner.png"}
         alt="Banner Image"
